6ab661d7c2163891ae8eb5f8afcf97eb6bd31d9c
Nikolas Görlitz <git@ngoerlitz.de>
2023-05-12 17:54:40 +0200

    prettier

--- config.ts
+++ config.ts
@@ -5,5 +5,7 @@ dotenv.config();
 export const Config = {
     APP_HOST: process.env.APP_HOST ?? '0.0.0.0',
     APP_PORT: Number(process.env.APP_PORT),
-    DATAFEED_REFRESH_INTERVAL_S: Number(process.env.DATAFEED_REFRESH_INTERVAL_S),
+    DATAFEED_REFRESH_INTERVAL_S: Number(
+        process.env.DATAFEED_REFRESH_INTERVAL_S
+    ),
 };
